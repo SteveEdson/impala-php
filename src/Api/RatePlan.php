b85ed0d88eb2ec7d3909881a481f0b4378e03727
Renato Santos <renato.miguel@gmail.com>
2018-07-27 22:11:33 +0100

    Added support to retrieve and update prices for rate plans

--- src/Api/RatePlan.php
+++ src/Api/RatePlan.php
@@ -2,6 +2,8 @@
 
 namespace Impala\Api;
 
+use \InvalidArgumentException;
+
 trait RatePlan
 {
     /**
@@ -24,5 +26,52 @@ trait RatePlan
     public function getRatePlanById(string $ratePlanId)
     {
         return $this->get('rate-plan/' . $ratePlanId);
+    }
+
+    /**
+     * Get the price of a rate plan.
+     *
+     * @param string $ratePlanId ID of the rate plan.
+     * @param array  $params     Params to be passed to request.
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getPriceForRatePlan(string $ratePlanId, array $params)
+    {
+        if (!isset($params['startDate']) || !isset($params['endDate'])) {
+            throw new InvalidArgumentException(
+                'getPriceForRatePlan requires both startDate and endDate'
+            );
+        }
+
+        $params['startDate'] = $this->formatDate($params['startDate']);
+        $params['endDate'] = $this->formatDate($params['endDate']);
+
+        return $this->get('rate-plan/' . $ratePlanId . '/price', $params);
+    }
+
+    /**
+     * Update the price of a rate plan.
+     *
+     * @param string $ratePlanId ID of the rate plan to update.
+     * @param array  $data       The updates to be applied to the rate plan.
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function updatePriceForRatePlan(string $ratePlanId, array $data)
+    {
+        if (!isset($data['date'])) {
+            throw new InvalidArgumentException(
+                'updatePriceForRatePlan requires date'
+            );
+        }
+
+        if (!isset($data['amountDescription'])) {
+            throw new InvalidArgumentException(
+                'updatePriceForRatePlan requires amountDescription'
+            );
+        }
+
+        $data['date'] = $this->formatDate($data['date']);
+
+        return $this->put('rate-plan/' . $ratePlanId . '/price', [], $data);
     }
 }
--- tests/Api/RatePlanTest.php
+++ tests/Api/RatePlanTest.php
@@ -18,11 +18,11 @@ class RatePlanTest extends TestCase
         $mock = $this->createApiMock();
 
         $mock->expects($this->once())
-             ->method('makeRequest')
-             ->with(
-                 $this->equalTo('GET'),
-                 $this->equalTo('hotel/hotelId/rate-plan')
-             );
+            ->method('makeRequest')
+            ->with(
+                $this->equalTo('GET'),
+                $this->equalTo('hotel/hotelId/rate-plan')
+            );
 
         $hotel = new Hotel('hotelId', $mock);
 
@@ -34,14 +34,170 @@ class RatePlanTest extends TestCase
         $mock = $this->createApiMock();
 
         $mock->expects($this->once())
-             ->method('makeRequest')
-             ->with(
-                 $this->equalTo('GET'),
-                 $this->equalTo('hotel/hotelId/rate-plan/ratePlanId')
-             );
+            ->method('makeRequest')
+            ->with(
+                $this->equalTo('GET'),
+                $this->equalTo('hotel/hotelId/rate-plan/ratePlanId')
+            );
 
         $hotel = new Hotel('hotelId', $mock);
 
         $hotel->getRatePlanById('ratePlanId');
+    }
+
+    public function testGetPriceForRatePlanCallsCorrectUrl()
+    {
+        $mock = $this->createApiMock();
+
+        $params = [
+            'startDate' => '2018-01-01',
+            'endDate' => '2018-01-02',
+        ];
+
+        $mock->expects($this->once())
+            ->method('makeRequest')
+            ->with(
+                $this->equalTo('GET'),
+                $this->equalTo('hotel/hotelId/rate-plan/ratePlanId/price'),
+                $this->equalTo(['query' => $params])
+            );
+
+        $hotel = new Hotel('hotelId', $mock);
+
+        $hotel->getPriceForRatePlan('ratePlanId', $params);
+    }
+
+    public function testErrorIsReturnedIfNoDatesArePassed()
+    {
+        $mock = $this->createApiMock();
+
+        $params = ['dummyParameter' => 'test'];
+
+        $hotel = new Hotel('hotelId', $mock);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $hotel->getPriceForRatePlan('ratePlanId', $params);
+    }
+
+    public function testErrorIsReturnedIfOnlyStartDateIsPassed()
+    {
+        $mock = $this->createApiMock();
+
+        $params = ['startDate' => '2018-01-01'];
+
+        $hotel = new Hotel('hotelId', $mock);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $hotel->getPriceForRatePlan('ratePlanId', $params);
+    }
+
+    public function testErrorIsReturnedIfOnlyEndDateIsPassed()
+    {
+        $mock = $this->createApiMock();
+
+        $params = ['endDate' => '2018-01-01'];
+
+        $hotel = new Hotel('hotelId', $mock);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $hotel->getPriceForRatePlan('ratePlanId', $params);
+    }
+
+    public function testDatesGetFormattedInGetPriceForRatePlan()
+    {
+        $mock = $this->createApiMock();
+
+        $params = [
+            'startDate' => '01-01-2018',
+            'endDate' => '02-01-2018',
+        ];
+
+        $mock->expects($this->once())
+            ->method('makeRequest')
+            ->with(
+                $this->equalTo('GET'),
+                $this->equalTo('hotel/hotelId/rate-plan/ratePlanId/price'),
+                $this->equalTo([
+                    'query' => [
+                        'startDate' => '2018-01-01',
+                        'endDate' => '2018-01-02',
+                    ]
+                ])
+            );
+
+        $hotel = new Hotel('hotelId', $mock);
+
+        $hotel->getPriceForRatePlan('ratePlanId', $params);
+    }
+
+    public function testupdatePriceForRatePlanCallsCorrectUrl()
+    {
+        $mock = $this->createApiMock();
+
+        $data = ['date' => '2018-01-01', 'amountDescription' => 'test'];
+
+        $mock->expects($this->once())
+            ->method('makeRequest')
+            ->with(
+                $this->equalTo('PUT'),
+                $this->equalTo('hotel/hotelId/rate-plan/ratePlanId/price'),
+                $this->equalTo([
+                    'json' => $data,
+                    'query' => []
+                ])
+            );
+
+        $hotel = new Hotel('hotelId', $mock);
+
+        $hotel->updatePriceForRatePlan('ratePlanId', $data);
+    }
+
+    public function testErrorIsReturnedIfDateIsNotPassed()
+    {
+        $mock = $this->createApiMock();
+
+        $data = ['amountDescription' => 'test'];
+
+        $hotel = new Hotel('hotelId', $mock);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $hotel->updatePriceForRatePlan('ratePlanId', $data);
+    }
+
+    public function testErrorIsReturnedIfAmountDescriptionIsNotPassed()
+    {
+        $mock = $this->createApiMock();
+
+        $data = ['date' => '2018-01-01'];
+
+        $hotel = new Hotel('hotelId', $mock);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $hotel->updatePriceForRatePlan('ratePlanId', $data);
+    }
+
+    public function testDateGetsFormattedInUpdatePriceForRatePlan()
+    {
+        $mock = $this->createApiMock();
+
+        $params = ['date' => '01-01-2018', 'amountDescription' => 'test'];
+
+        $mock->expects($this->once())
+            ->method('makeRequest')
+            ->with(
+                $this->equalTo('PUT'),
+                $this->equalTo('hotel/hotelId/rate-plan/ratePlanId/price'),
+                $this->equalTo([
+                    'json' => [
+                        'date' => '2018-01-01',
+                        'amountDescription' => 'test'
+                    ],
+                    'query' => [],
+                ])
+            );
+
+        $hotel = new Hotel('hotelId', $mock);
+
+        $hotel->updatePriceForRatePlan('ratePlanId', $params);
     }
 }
